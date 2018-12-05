7830901a513b05ee2bd1b2f2491e175c44d3a86c
CarsonRey <c.rey494@gmail.com>
2018-12-05 16:20:10 -0500

    user can save drinks and view saved drinks

--- app/controllers/user_drinks_controller.rb
+++ app/controllers/user_drinks_controller.rb
@@ -1,2 +1,23 @@
 class UserDrinksController < ApplicationController
+
+  def index
+    @associations = UserDrink.all
+    render json: @associations
+  end
+
+  def create
+    @userdrink = UserDrink.create(user_id: params[:user_id], drink_id: params[:drink_id])
+  end
+
+  def show
+    @userdrink = UserDrink.find(params[:id])
+    render json: @userdrink
+  end
+
+  # private
+  #
+  # def user_drink_params
+  #    params.permit(:user_id, :drink_id)
+  # end
+
 end
--- app/serializers/user_drink_serializer.rb
+++ app/serializers/user_drink_serializer.rb
@@ -1,3 +1,3 @@
 class UserDrinkSerializer < ActiveModel::Serializer
-  
+  attributes :id, :user_id, :drink_id
 end
